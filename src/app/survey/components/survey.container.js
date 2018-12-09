dc39016b66ceae48bf560d159bd548b1f3dd141f
Costa <ccavallo@enetworks.it>
2018-12-09 17:57:55 +0100

    survey UI

--- src/app/survey/components/survey.container.js
+++ src/app/survey/components/survey.container.js
@@ -24,13 +24,8 @@ class Survey extends Component {
             <div className="card mb-4 shadow-sm">
               <div className="card-body">
                 <p className="card-text">{question.title}</p>
-                { this.renderAnswers(answers,question.type) }
+                { this.renderAnswers(answers,question.type,question.id) }
                 <div className="d-flex justify-content-between align-items-center">
-                  <div className="btn-group">
-                    <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small className="text-muted">9 mins</small>
                 </div>
               </div>
             </div>
@@ -38,14 +33,38 @@ class Survey extends Component {
            )
        })
     }
-    renderAnswers = (answers, type) => {
+    renderAnswers = (answers, type, questionID) => {
        switch(type) {
            case OPEN :
               return (
                 <div className="form-group">
                     <textarea className="form-control mt-2" id="text_response" rows="3"></textarea>
                 </div>
-            ) 
+            )
+            case MULTIPLE:
+                return(
+                    answers.map((item) => {
+                    return(
+                        <div key={item.id} className="form-check mt-1">
+                            <input className="form-check-input" type="checkbox" value={item.id} id={item.id} />
+                            <label className="form-check-label" htmlFor={item.id}>
+                                {item.response}
+                            </label>
+                        </div>)
+                    })
+                )
+            case EXCLUSIVE :
+            return(
+                answers.map((item) => {
+                return(
+                    <div key={item.id} className="form-check mt-1">
+                       <input className="form-check-input" type="radio" name={questionID} id={item.id} value="option1"/>
+                        <label className="form-check-label" htmlFor={item.id}>
+                            Default radio
+                        </label>
+                    </div>)
+                })
+            )
             
        }
     }
@@ -54,7 +73,7 @@ class Survey extends Component {
     return (
         <Container className="mt-2">
            <h2>Survey</h2>
-           { survey ? this.renderSurvey() : (<a>No survey</a>) }
+           { survey ? this.renderSurvey() : (null)}
         </Container>
      )
   }
