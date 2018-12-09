a6a2ad67e3f9bbf971a6d72a22ab2155a57613d9
Costa <ccavallo@enetworks.it>
2018-12-09 21:49:03 +0100

    redux-form + redux downgrade

--- src/app/survey/components/survey.container.js
+++ src/app/survey/components/survey.container.js
@@ -3,8 +3,11 @@ import { connect } from 'react-redux';
 import { getSurvey } from '../actions/survey.actions';
 import { Container } from 'reactstrap';
 import { GET_SURVEY_REQUEST } from '../sagas/survey.sagas';
+import { Field, reduxForm } from 'redux-form';
 
-
+const validateComment = (value, allValues, props) => {
+    if (!value) return "Can't be empty."
+  }
 const OPEN = 'open';
 const EXCLUSIVE = 'exclusive';
 const MULTIPLE = 'multiple';
@@ -17,30 +20,43 @@ class Survey extends Component {
     componentDidMount() {
        this.props.onRequestSurvey();
     }
-    renderSurvey= () => {
-       return this.props.survey.map((pack) => {
-           const {question, answers} = pack;
-           return (
-           <div key={question.id} className="col-md-12">
-            <div className="card mb-4 shadow-sm">
-              <div className="card-body">
-                <p className="card-text">{question.title}</p>
-                { this.renderAnswers(answers,question.type,question.id) }
-                <div className="d-flex justify-content-between align-items-center">
-                </div>
-              </div>
-            </div>
-          </div>
-           )
-       })
+    renderSurvey = () => {
+        const { error, handleSubmit, pristine, reset, submitting } = this.props;
+        return(
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                {this.renderPack()}
+                <button type="submit" disabled={submitting} className="btn">Submit</button>
+            </form>
+        )
     }
+    renderPack = () => {
+        return(
+            this.props.survey.map((pack) => {
+                const {question, answers} = pack;
+                return (
+                <div key={question.id} className="col-md-12">
+                    <div className="card mb-4 shadow-sm">
+                    <div className="card-body">
+                        <p className="card-text">{question.title}</p>
+                        { this.renderAnswers(answers,question.type,question.id) }
+                        <div className="d-flex justify-content-between align-items-center">
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                )
+            })
+        )
+    }
+  
     renderAnswers = (answers, type, questionID) => {
        switch(type) {
            case OPEN :
               return (
-                <div className="form-group">
+                <Field name={questionID + "s"} validate={validateComment}component={this.renderTextArea} />
+               /* <div className="form-group">
                     <textarea className="form-control mt-2" id="text_response" rows="3"></textarea>
-                </div>
+                </div> */
             )
             case MULTIPLE:
                 return(
@@ -61,13 +77,29 @@ class Survey extends Component {
                     <div key={item.id} className="form-check mt-1">
                        <input className="form-check-input" type="radio" name={questionID} id={item.id} value="option1"/>
                         <label className="form-check-label" htmlFor={item.id}>
-                            Default radio
+                            {item.response}
                         </label>
                     </div>)
                 })
             )
             
        }
+    }
+    renderTextArea(field) {
+        const { meta : { touched,error }} = field;
+        const className = "form-group " +  (touched && error) ? 'has-error' : '' ;
+        return(
+            <div className={className}>
+            <a>Sticaz</a>
+                <textarea className="form-control mt-2" id="text_response" rows="3" {...field.input}></textarea>
+                <div className="text-help">
+                 {touched ? error : ''}
+                </div>
+            </div>
+        )
+    }
+    onSubmit(values) {
+        console.log(values);
     }
    render(){
     const { fetching, survey, onRequestSurvey, error } = this.props;
@@ -80,6 +112,13 @@ class Survey extends Component {
   }
 }
 
+function validate(values) {
+    const errors = {};
+    if(!values.title) {
+        errors.title = "Required";
+    }
+    return errors;
+  }
 const mapStateToProps = state => {
     return {
       fetching: state.survey.fetching,
@@ -94,4 +133,8 @@ const mapStateToProps = state => {
     };
   };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Survey);
+//export default  connect(mapStateToProps, mapDispatchToProps)(Survey);
+
+export default  reduxForm({
+    form : 'SurveyForm'
+})(connect(mapStateToProps, mapDispatchToProps)(Survey));
